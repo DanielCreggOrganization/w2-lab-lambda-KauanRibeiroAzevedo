243ddf101e747f070952d2eece5fe1c53e0bd408
KauanRibeiroAzevedo <145453537+KauanRibeiroAzevedo@users.noreply.github.com>
2025-01-27 14:06:48 +0000

    Creating and Using an Interface lambda

--- src/ie/atu/lambda/Main.java
+++ src/ie/atu/lambda/Main.java
@@ -2,14 +2,15 @@ package ie.atu.lambda;
 
 public class Main {
     public static void main(String[] args) {
-        // Create an instance of ToUpperCase
-        StringOperation toUpperCase = new ToUpperCase();
+        // Using a lambda expression to implement StringOperation
+        StringOperation toUpperCase = (String input) -> input.toUpperCase();
 
-        // Test it with a string
+        // Test the lambda with a string
         String result = toUpperCase.apply("hello world");
 
-        
+        // Print the result
         System.out.println(result); 
     }
 }
+
 
